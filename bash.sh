81ca0f43c632f4d4aabcbc1183033c8c318dc759
T2607 <tahera26@hotmail.co.uk>
2022-04-01 13:56:59 +0000

    updating script

--- bash.sh
+++ bash.sh
@@ -2,6 +2,7 @@
 
 #test application
 echo "installing dependencies and running tests"
+sleep 2
 npm install
 npm test
 
@@ -10,3 +11,4 @@ echo "building docker image"
 #delays for 2  seconds
 sleep 2
 docker build -t myapp:latest .
+docker system prune -y
